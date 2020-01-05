eae64f594d2108ca4b9cea04603fbd367eaee77a
Magno Sescon <magnosescon@gmail.com>
2020-01-05 08:57:19 -0300

    First Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { DashboardModule} from './dashboard';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports:[
+        DashboardModule
+      ]
     }).compileComponents();
   }));
 
@@ -16,16 +20,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dashboard'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('dashboard');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dashboard app is running!');
-  });
+  
 });
